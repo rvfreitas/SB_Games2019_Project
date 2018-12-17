adff7296f7541c2861ed90d2cddb1f7725fdfbfc
1513_IRON\1513 IRON <gabrielfritter@gmail.com>
2018-12-17 17:07:56 -0200

    Commit with the movement concept for the game

---
+++ SBGames Test/Assets/Scripts/Aim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour {
+
+    public GameObject aim;
+
+	// Use this for initialization
+	void Start () {
+
+        Cursor.visible = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 mouseScreen = Camera.main.ScreenToWorldPoint(Input.mousePosition -this.transform.position );
+        this.transform.up = mouseScreen;
+
+        aim.transform.position = mouseScreen;
+		
+	}
+}
